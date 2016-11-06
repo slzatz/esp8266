e15f1e1eb959d9116dc10a4601946e06e73c0761
slzatz <slzatz@gmail.com>
2016-11-06 15:54:35 -0500

    conway.py: added a list of lists to hold the colors (color_board) - seems to work although the colors change and so there is another version that handles colors more thoughtfully

--- conway.py
+++ conway.py
@@ -1,21 +1,20 @@
-from machine import Pin
+from machine import Pin, freq
 import neopixel
 import utime
 import uos
+
+freq(160000000)
 
 PIXEL_WIDTH = const(8)
 PIXEL_HEIGHT = const(8)
 MAX_BRIGHT = const(2)
 
-# note that the + 1 below means that the board[X][-1] pulls in zero from a position beyond the grid
-board = [[0 for j in range(PIXEL_WIDTH + 1)] for i in range(PIXEL_HEIGHT + 1)]
-new_board = [[0 for j in range(PIXEL_WIDTH + 1)] for i in range(PIXEL_HEIGHT + 1)]
-
 pixels = neopixel.NeoPixel(Pin(13, Pin.OUT), PIXEL_WIDTH*PIXEL_HEIGHT)
 
 def conway_step():
   global board
-  global new_board
+  global color_board
+  new_board = board[:]
   changed = False
   for x in range(PIXEL_HEIGHT):
     for y in range(PIXEL_WIDTH):
@@ -24,21 +23,44 @@ def conway_step():
 
       if board[x][y] and not (2 <= num_neighbors <=3):
         new_board[x][y] = 0
+        color_board[x][y] = (0,0,0) 
         changed = True
       elif not board[x][y] and num_neighbors == 3:
         new_board[x][y] = 1
+        color_board[x][y] = color() 
         changed = True
   
   board = new_board[:]
   return changed
 
 def conway_rand():
+  global board
+  global color_board
+
+  pixels.fill((0,0,0))
+  pixels.write()
+  utime.sleep(2)
+
+  # note that the + 1 below means that the board[X][-1] pulls in zero from a position beyond the grid
+  board = [[0 for j in range(PIXEL_WIDTH + 1)] for i in range(PIXEL_HEIGHT + 1)]
+  color_board = [[(0,0,0) for j in range(PIXEL_WIDTH)] for i in range(PIXEL_HEIGHT)]
   for x in range(PIXEL_HEIGHT):
     for y in range(PIXEL_WIDTH):
       board[x][y] = uos.urandom(1)[0]//128
+      if board[x][y]:
+        color_board[x][y] = color()
+      else:
+        color_board[x][y] = (0,0,0)
 
-pixels.fill((0,0,0))
-pixels.write()
+  print("--------------------------------------------")
+  for x in range(PIXEL_HEIGHT):
+    print(board[x][0:PIXEL_WIDTH])
+    for y in range(PIXEL_WIDTH):
+        pixels[x * PIXEL_HEIGHT + y] = color_board[x][y]
+  print("--------------------------------------------")
+
+  pixels.write()
+  utime.sleep(3)
 
 def random():
   return MAX_BRIGHT*(uos.urandom(1)[0]//128)
@@ -46,21 +68,20 @@ def random():
 def color():
   return (random(), random(), 2)
 
+conway_rand()
+
 while True:
   if not conway_step():
-    utime.sleep(10)
+    utime.sleep(5)
     conway_rand()
 
   print("--------------------------------------------")
   for x in range(PIXEL_HEIGHT):
     print(board[x][0:PIXEL_WIDTH])
     for y in range(PIXEL_WIDTH):
-      if board[x][y]:
-        pixels[x * PIXEL_HEIGHT + y] = color()
-      else:
-        pixels[x * PIXEL_HEIGHT + y] = (0,0,0)
-
+        pixels[x * PIXEL_HEIGHT + y] = color_board[x][y]
   print("--------------------------------------------")
+
   pixels.write()
   utime.sleep(0.1)
 
