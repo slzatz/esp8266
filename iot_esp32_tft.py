1bd6a710748f8048c105a5e151b05454a1de8ea3
T470-slz <slzatz@gmail.com>
2017-10-10 20:35:31 -0400

    iot_esp32_tft.py: very much work in progress to display lines of text -- need to work on regex since findall and finditer don't exist

--- iot_esp32_tft.py
+++ iot_esp32_tft.py
@@ -26,6 +26,8 @@ print("topic =", topic)
 
 tft = display.TFT()
 tft.init(tft.ILI9341, width=240, height=320, miso=19, mosi=18, clk=5, cs=15, dc=33, bgr=True)
+utime.sleep(1)
+tft.clear()
 tft.text(10, 10, "Hello Steve", random(0xFFFFFF))
 
 #pin15 = Pin(15, Pin.OUT) #will need to find another pin since this is cs pin
@@ -45,6 +47,11 @@ def wrap(text,lim):
 
   lines.append(' '.join(line))
   return lines
+
+line_height = tft.fontSize()[1]
+MAX_HEIGHT = 320
+max_chars_line = 40         
+indent = 17
 
 def conncb(task):
   print("[{}] Connected".format(task))
@@ -66,18 +73,85 @@ def datacb(msg):
     zz = {}
   #msg = zz.get('message', '')
   t = "{}".format(utime.strftime("%c", utime.localtime()))
+  bullets = zz.get('bullets', True)
+
 
   tft.clear()
   tft.text(0, 0, msg[1], random(0xFFFFFF))
   tft.text(0, 12, t, random(0xFFFFFF))
   #tft.text(0, 24, msg, 0x00FF00))
 
-  y = 24
-  for line in zz.get('text', ["No text"]):
-    lines = wrap(line, 26)
-    for l in lines:
-      y+=15 
-      tft.text(0, y, l, 0x00FF00))
+  #y = 24
+  #for line in zz.get('text', ["No text"]):
+  #  lines = wrap(line, 26)
+  #  for l in lines:
+  #    y+=15 
+  #    tft.text(0, y, l, 0x00FF00))
+#############################
+
+  n = line_height #20
+  for item in zz.get('text',['No text']): 
+    if not item.strip():
+      n+=line_height
+      continue
+    #font.set_bold(False)
+    #max_chars_line = 40        
+    #indent = 17
+    n+=4 if bullets else 0 # makes multi-line bullets more separated from prev and next bullet
+
+    if n+line_height > MAX_HEIGHT:
+      break
+
+    if item[0] == '#':
+      item=item[1:]
+      #font.set_bold(True)
+      #max_chars_line = 60
+
+    if item[0] == '*': 
+      #foo.blit(star, (2,n+7))
+      item=item[1:]
+    elif bullets:
+      #foo.blit(bullet_surface, (7,n+13)) #(4,n+13)
+      pass
+    # neither a star in front of item or a bullet
+    else:
+      #max_chars_line+= 1 
+      #indent = 10
+      pass
+
+    print("1")
+    print(item)
+    print("2")
+    lines = wrap(item, max_chars_line) # if line is just whitespace it returns []
+    print(lines)
+
+    #for l,line in enumerate(lines):
+
+    #  if n+line_height > MAX_HEIGHT: #20
+    #    break
+
+    #  if l:
+    #    phrases = get_phrases(line, phrase[0])
+    #  else:
+    #    phrases = get_phrases(line)
+
+    #  xx = 0
+    #  for phrase in phrases:
+    #    try:
+    #      text = font.render(phrase[1], antialias, color_map.get(phrase[0], (255,255,255))) 
+    #      tft.text(indent+xx, n+5, phrase[1], 0x00FF00)
+    #    except UnicodeError as e:
+    #      print("UnicodeError in text lines: "+e)
+    #      continue
+    #    #foo.blit(text, (indent + xx,n+5))
+    #    #xx+=text.get_rect().width
+    #    xx+=tft.textWidth(text)
+
+    #  line_widths.append(xx)
+    for line in lines:
+      tft.text(indent, n+5, line, 0x00FF00)
+      n+=line_height
+#############################
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
@@ -93,7 +167,7 @@ rtc = RTC()
 print("synchronize time with NTP server ...")
 # limit the time waiting since sometimes never connects
 rtc.ntp_sync(server="pool.ntp.org")
-for n in range(10):
+for k in range(10):
   if rtc.synced():
     break
   utime.sleep_ms(100)
