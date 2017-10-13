dea396339a3205f4c21b4d423e0945123a083768
T470-slz <slzatz@gmail.com>
2017-10-12 21:33:53 -0400

    iot_esp32_tft.py: uses simple markup to color the text -- works but needs a lot of cleanup

--- iot_esp32_tft.py
+++ iot_esp32_tft.py
@@ -12,6 +12,7 @@ import network, utime
 import display
 from machine import Pin, I2C, RTC, random
 import json
+import ure as re
 from config import ssid, pw, mqtt_aws_host
 
 with open('mqtt_id', 'r') as f:
@@ -31,6 +32,34 @@ tft.clear()
 tft.text(10, 10, "Hello Steve", random(0xFFFFFF))
 
 #pin15 = Pin(15, Pin.OUT) #will need to find another pin since this is cs pin
+
+regex= re.compile('{(.*?)}')
+#s = "jkdsfl{RED}fkjsdflds{GREEN}jlklfjsl{PINK}lkdsjflkdsjfl"
+def display_text(s, n, tag=None, h=0):
+
+  # the following two things can only happen the first time a string is processed
+  if s[0] != '{': # deal with strings with no pos 0 tag (they may be tags elsewhere in the string)
+    s = '{WHITE}' + s
+  if tag is None: 
+    z = regex.search(s)
+    tag = z.group(0)
+  
+  col = tag[1:-1]
+  s = s[len(tag):]
+  z = regex.search(s)
+  if z is None:
+    print("No more tags")
+    print("col=",col)
+    print("text=",s)
+    tft.text(h, n, s, getattr(tft, col))
+    return
+  tag2 = z.group(0)
+  pos = s.find(tag2) # will be non-zero
+  print("col=",col)
+  print("text=",s[:pos])
+  tft.text(h, n, s[:pos], getattr(tft, col))
+  h+=tft.textWidth(s[:pos])
+  return display_text(s[pos:], n, tag2, h)
 
 def wrap(text,lim):
   lines = []
@@ -77,17 +106,9 @@ def datacb(msg):
 
 
   tft.clear()
-  tft.text(0, 0, msg[1], random(0xFFFFFF))
-  tft.text(0, 12, t, random(0xFFFFFF))
+  #tft.text(0, 0, msg[1], random(0xFFFFFF))
+  #tft.text(0, 12, t, random(0xFFFFFF))
   #tft.text(0, 24, msg, 0x00FF00))
-
-  #y = 24
-  #for line in zz.get('text', ["No text"]):
-  #  lines = wrap(line, 26)
-  #  for l in lines:
-  #    y+=15 
-  #    tft.text(0, y, l, 0x00FF00))
-#############################
 
   n = line_height #20
   for item in zz.get('text',['No text']): 
@@ -119,37 +140,12 @@ def datacb(msg):
       #indent = 10
       pass
 
-    print("1")
-    print(item)
-    print("2")
+    print("item=",item)
     lines = wrap(item, max_chars_line) # if line is just whitespace it returns []
-    print(lines)
+    print("lines=",lines)
 
-    #for l,line in enumerate(lines):
-
-    #  if n+line_height > MAX_HEIGHT: #20
-    #    break
-
-    #  if l:
-    #    phrases = get_phrases(line, phrase[0])
-    #  else:
-    #    phrases = get_phrases(line)
-
-    #  xx = 0
-    #  for phrase in phrases:
-    #    try:
-    #      text = font.render(phrase[1], antialias, color_map.get(phrase[0], (255,255,255))) 
-    #      tft.text(indent+xx, n+5, phrase[1], 0x00FF00)
-    #    except UnicodeError as e:
-    #      print("UnicodeError in text lines: "+e)
-    #      continue
-    #    #foo.blit(text, (indent + xx,n+5))
-    #    #xx+=text.get_rect().width
-    #    xx+=tft.textWidth(text)
-
-    #  line_widths.append(xx)
     for line in lines:
-      tft.text(indent, n+5, line, 0x00FF00)
+      display_text(line, n)
       n+=line_height
 #############################
 
